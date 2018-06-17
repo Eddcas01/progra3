2e44c973275c7251c9a6ec10eae9867eda22b09b
Gustavo <ggusperezg@gmail.com>
2018-06-17 15:11:52 -0600

    Programa base

---
+++ PROGRA 3/PROGRA 3/Source.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() 
+{
+	int opmain = 0;
+	do
+	{
+		system("cls");
+		cout << "\n\t\t################# - MENU - ################\n";
+		cout << "\t\t# 1.                                        #\n";
+		cout << "\t\t# 2.                                        #\n";
+		cout << "\t\t# 3.                                        #\n";
+		cout << "\t\t# 4.                                        #\n";
+		cout << "\t\t# 5.                                        #\n";
+		cout << "\t\t# 6.                                        #\n";
+		cout << "\t\t# 7.                                        #\n";
+		cout << "\t\t# 8.                                        #\n";
+		cout << "\t\t# 9.                                        #\n";
+		cout << "\t\t# 10.                                       #\n";
+		cout << "\t\t#############################################\n";
+		cout << "\t\tseleccione una opcion -> ";
+		cin >> opmain;
+
+		switch (opmain)
+		{
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		case 4: break;
+		case 5: break;
+		case 6: break;
+		case 7: break;
+		case 8: break;
+		case 9: break;
+		case 10: break;
+
+		default:
+			break;
+		}
+	} while (opmain!=0);
+	
+	
+	system("pause>nul");
+
+}
